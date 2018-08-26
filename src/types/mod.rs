182055ccf39e4568c1c71d8727eab8eb4462518a
Andrew Plaza <aplaza@liquidthink.net>
2018-08-26 12:06:59 -0400

    export more trace types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -21,7 +21,7 @@ pub use self::transaction_id::TransactionId;
 pub use self::transaction_request::{CallRequest, TransactionCondition, TransactionRequest};
 pub use self::uint::{H128, H160, H2048, H256, H512, H520, H64, U128, U256, U64};
 pub use self::work::Work;
-pub use self::trace_filtering::{Trace, TraceFilter, TraceFilterBuilder, Res, Action, CallType, Call, Create, Suicide, Reward};
+pub use self::trace_filtering::{Trace, TraceFilter, TraceFilterBuilder, Res, Action, CallType, Call, Create, Suicide, Reward, CallResult, CreateResult};
 pub use self::traces::{TraceType, BlockTrace, TransactionTrace};
 
 /// Address
